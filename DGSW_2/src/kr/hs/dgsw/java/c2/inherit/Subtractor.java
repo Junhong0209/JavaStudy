b56d4249c88ce206459aaafa5c5c235911171ddb
Junhong0209 <junh040209@naver.com>
2021-06-01 14:17:53 +0900

    2021.3.31 수업 내용

---
+++ DGSW_2/src/kr/hs/dgsw/java/c2/inherit/Subtractor.java
@@ -0,0 +1,22 @@
+package kr.hs.dgsw.java.c2.inherit;
+
+public class Subtractor extends Adder{
+	
+	@Override
+	public int calculate() {
+		return getOperand1() - getOperand2();
+	}
+	
+	@Override
+	public String getOperator() {
+		return "-";
+	}
+
+	public static void main(String[] args) {
+		Subtractor adder = new Subtractor();
+		adder.setOperand1(4914);
+		adder.setOperand2(9828);
+		
+		adder.display();
+	}
+}
